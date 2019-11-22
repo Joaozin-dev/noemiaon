b994f1e89eb061bde0ffd1db80216283292569c4
= <=>
2019-11-22 20:56:46 -0300

    HEHE

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.set('views',path.join(__dirname,'public'));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
 
+app.get('/',(req,res)=>{
+	res.render("index.html");
+});
+
 io.on('connection',function(socket){
 	console.log(`Socket conectado ${socket.id}`); 
 	app.get('/login/:user',function(req,res){
